47b358462a09e136da7def1b034a6ea03ad3b053
Z00DPDR <ayush.anand2123@gmail.com>
2024-02-22 10:43:04 +0530

    demo

---
+++ src/Exception.java
@@ -0,0 +1,26 @@
+//public class Exception {
+//    public static void validate(int age) {
+//        if(age<18) {
+//            throw new ArithmeticException("Person is not eligible to vote");
+//        }
+//        else {
+//            System.out.println("Person is eligible to vote!!");
+//        }
+//    }
+//    public static void main(String args[]){
+//        validate(13);
+//        System.out.println("rest of the code...");
+//    }
+//}
+
+
+import java.io;
+import java.io.FileNotFoundException;
+
+public class TestThrow2{
+    public static void method()throws FileNotFoundException{
+        FileReader file = new FileReader()
+    }
+}
+
+
